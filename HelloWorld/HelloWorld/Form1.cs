66a1985363a3052538116da26135c391d6d32b38
Julian De La Roche <Juliand@peacocks.com.au>
2020-12-03 14:28:31 +1100

    Small change

--- HelloWorld/HelloWorld/Form1.cs
+++ HelloWorld/HelloWorld/Form1.cs
@@ -19,7 +19,7 @@ namespace HelloWorld
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Welcome to this application Julian");
+            MessageBox.Show("Welcome to this application Julian", "Hello World");
         }
 
         private void label1_Click(object sender, EventArgs e)
